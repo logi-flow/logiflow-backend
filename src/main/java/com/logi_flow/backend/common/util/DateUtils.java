e2d79249ca627f50ea436365c62d2ec64a1981c8
buzz9248 <buzz9248@naver.com>
2025-08-07 12:46:44 +0900

    feat: DateUtils 구현

--- src/main/java/com/logi_flow/backend/common/util/DateUtils.java
+++ src/main/java/com/logi_flow/backend/common/util/DateUtils.java
@@ -1,4 +1,28 @@
 package com.logi_flow.backend.common.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class DateUtils {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter YEAR_MONTH_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM");
+
+    public static String format(LocalDateTime dateTime) {
+        return (dateTime != null) ? dateTime.format(FORMATTER) : null;
+    }
+
+    public static LocalDateTime parse(String date) {
+        return (date != null && !date.isEmpty()) ? LocalDateTime.parse(date, FORMATTER) : null;
+    }
+
+    public static String yearDateFormat(LocalDateTime dateTime) {
+        return (dateTime != null) ? dateTime.format(YEAR_MONTH_FORMATTER) : null;
+    }
+
+    public static LocalDateTime yearDateParse(String dateString) {
+        if(dateString == null || dateString.isEmpty()) return null;
+
+        String fixed = dateString + "-01T00:00:00";
+        return LocalDateTime.parse(fixed);
+    }
 }
