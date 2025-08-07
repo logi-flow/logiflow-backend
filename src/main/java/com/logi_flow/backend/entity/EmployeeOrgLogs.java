1a594c07e10520ffad86f0cb17aea1d529d44cfc
bbang-2 <yys799@nate.com>
2025-08-07 12:08:43 +0900

    feat: 사용자 로그 엔티티 구현

---
+++ src/main/java/com/logi_flow/backend/entity/EmployeeOrgLogs.java
@@ -0,0 +1,40 @@
+package com.logi_flow.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "employees_org_logs")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class EmployeeOrgLogs extends BaseTimeLog{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "changed_by")
+    private User user;
+
+    @Column(name = "changed_by_username", nullable = false)
+    private String changedByUsername;
+
+    @Column(name = "change_reason")
+    private String changeReason;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    @Column(name = "prev_data")
+    private String prevData;
+
+    @Column(name = "new_data")
+    private String newData;
+}
