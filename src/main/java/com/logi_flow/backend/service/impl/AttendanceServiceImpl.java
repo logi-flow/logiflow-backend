4ef2a562b8c1193d468ee0561ce26b6d3071cbfb
mijinn0 <pmj_0818@naver.com>
2025-08-11 17:13:16 +0900

    feat: 출근부 controller 생성

--- src/main/java/com/logi_flow/backend/service/impl/AttendanceServiceImpl.java
+++ src/main/java/com/logi_flow/backend/service/impl/AttendanceServiceImpl.java
@@ -1,5 +1,11 @@
 package com.logi_flow.backend.service.impl;
 
+import com.logi_flow.backend.config.security.UserPrincipal;
+import com.logi_flow.backend.dto.ResponseDto;
+import com.logi_flow.backend.dto.attendance.response.CreateAttendanceResponseDto;
+import com.logi_flow.backend.dto.attendance.response.GetAllAttendanceResponseDto;
+import com.logi_flow.backend.dto.attendance.response.GetMyAttendancesResponseDto;
+import com.logi_flow.backend.dto.attendance.response.UpdateAttendanceResponseDto;
 import com.logi_flow.backend.service.AttendanceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,4 +13,28 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class AttendanceServiceImpl implements AttendanceService {
+    @Override
+    public ResponseDto<CreateAttendanceResponseDto> checkInAttendance(UserPrincipal userPrincipal) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<UpdateAttendanceResponseDto> checkOutAttendance(UserPrincipal userPrincipal) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<GetAllAttendanceResponseDto> getAllAttendance() {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<GetAllAttendanceResponseDto> getAttendanceDetails(Long attendanceId) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<GetMyAttendancesResponseDto> getMyAttendances(UserPrincipal userPrincipal) {
+        return null;
+    }
 }
