fccd75110cdf0598d0b09317f9f0dabb0ff4b329
chang0506 <j8970@naver.com>
2025-08-19 16:28:27 +0900

    feat:assignment-service

--- src/main/java/com/logi_flow/backend/service/AssignmentService.java
+++ src/main/java/com/logi_flow/backend/service/AssignmentService.java
@@ -8,12 +8,14 @@ import com.logi_flow.backend.dto.assignment.response.GetAllAssignmentResponseDto
 import com.logi_flow.backend.dto.assignment.response.GetAssignmentDetailResponseDto;
 import com.logi_flow.backend.dto.assignment.response.UpdateAssignmentResponseDto;
 
+import java.util.List;
+
 public interface AssignmentService {
     ResponseDto<CreateAssignmentResponseDto> createAssignment(CreateAssignmentRequestDto dto);
 
     ResponseDto<UpdateAssignmentResponseDto> updateAssignment(Long assignmentId, UpdateAssignmentRequestDto dto);
 
-    ResponseDto<GetAllAssignmentResponseDto> getAllAssignment();
+    ResponseDto<List<GetAllAssignmentResponseDto>> getAllAssignment();
 
     ResponseDto<GetAssignmentDetailResponseDto> getAssignmentDetail(Long assignmentId);
 
--- src/main/java/com/logi_flow/backend/service/impl/AssignmentServiceImpl.java
+++ src/main/java/com/logi_flow/backend/service/impl/AssignmentServiceImpl.java
@@ -1,5 +1,9 @@
 package com.logi_flow.backend.service.impl;
 
+import com.logi_flow.backend.common.constants.ResponseCode;
+import com.logi_flow.backend.common.constants.ResponseMessage;
+import com.logi_flow.backend.common.enums.AssignmentStatus;
+import com.logi_flow.backend.common.enums.driver.VehicleStatus;
 import com.logi_flow.backend.dto.ResponseDto;
 import com.logi_flow.backend.dto.assignment.request.CreateAssignmentRequestDto;
 import com.logi_flow.backend.dto.assignment.request.UpdateAssignmentRequestDto;
@@ -7,16 +11,68 @@ import com.logi_flow.backend.dto.assignment.response.CreateAssignmentResponseDto
 import com.logi_flow.backend.dto.assignment.response.GetAllAssignmentResponseDto;
 import com.logi_flow.backend.dto.assignment.response.GetAssignmentDetailResponseDto;
 import com.logi_flow.backend.dto.assignment.response.UpdateAssignmentResponseDto;
+import com.logi_flow.backend.entity.Assignment;
+import com.logi_flow.backend.entity.Driver;
+import com.logi_flow.backend.entity.Vehicle;
+import com.logi_flow.backend.repository.AssignmentRepository;
+import com.logi_flow.backend.repository.DriverRepository;
+import com.logi_flow.backend.repository.VehicleRepository;
 import com.logi_flow.backend.service.AssignmentService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class AssignmentServiceImpl implements AssignmentService {
+
+    private final AssignmentRepository assignmentRepository;
+    private final DriverRepository driverRepository;
+    private final VehicleRepository vehicleRepository;
+
     @Override
     public ResponseDto<CreateAssignmentResponseDto> createAssignment(CreateAssignmentRequestDto dto) {
-        return null;
+        CreateAssignmentResponseDto data = null;
+
+        Driver driver = driverRepository.findById(dto.getDriverId())
+                .orElseThrow(() -> new EntityNotFoundException(ResponseMessage.USER_NOT_FOUND));
+
+        Vehicle vehicle = vehicleRepository.findById(dto.getVehicleId())
+                .orElseThrow(() -> new EntityNotFoundException(ResponseMessage.RESOURCE_NOT_FOUND));
+
+        if (vehicle.getStatus() != VehicleStatus.AVAILABLE) {
+            throw new IllegalArgumentException(ResponseMessage.FAILED);
+        }
+
+        if (assignmentRepository.existsByVehicleAndStatus(vehicle, AssignmentStatus.ACTIVE)) {
+            throw new IllegalArgumentException(ResponseMessage.FAILED);
+        }
+
+        Assignment newAssignment = Assignment.builder()
+                .driver(driver)
+                .vehicle(vehicle)
+                .isPrimary(dto.getIsPrimary())
+                .status(AssignmentStatus.ACTIVE)
+                .build();
+
+        vehicle.setStatus(VehicleStatus.IN_USE);
+
+        assignmentRepository.save(newAssignment);
+
+        data = CreateAssignmentResponseDto.builder()
+                .id(newAssignment.getId())
+                .driverId(newAssignment.getDriver().getId())
+                .driverName(newAssignment.getDriver().getName())
+                .vehicleIds(newAssignment.getVehicle().getId())
+                .isPrimary(newAssignment.isPrimary())
+                .status(newAssignment.getStatus())
+                .createdAt(newAssignment.getCreatedAt())
+                .updatedAt(newAssignment.getUpdatedAt())
+                .build();
+
+        return ResponseDto.success(ResponseCode.SUCCESS, ResponseMessage.SUCCESS, data);
     }
 
     @Override
@@ -25,7 +81,7 @@ public class AssignmentServiceImpl implements AssignmentService {
     }
 
     @Override
-    public ResponseDto<GetAllAssignmentResponseDto> getAllAssignment() {
+    public ResponseDto<List<GetAllAssignmentResponseDto>> getAllAssignment() {
         return null;
     }
 
