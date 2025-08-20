1aafd860c52a0a983797c213d77805193c421826
kohyeokjae <kohj0686@naver.com>
2025-08-20 14:06:10 +0900

    refactor: delivery-service

--- src/main/java/com/logi_flow/backend/repository/ContractRepository.java
+++ src/main/java/com/logi_flow/backend/repository/ContractRepository.java
@@ -1,9 +1,15 @@
 package com.logi_flow.backend.repository;
 
+import com.logi_flow.backend.common.enums.ContractStatus;
 import com.logi_flow.backend.entity.Contract;
+import com.logi_flow.backend.entity.Customer;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ContractRepository extends JpaRepository<Contract, Long> {
+    Optional<Contract> findByCustomerAndStatus(Customer customer, ContractStatus contractStatus);
 }
