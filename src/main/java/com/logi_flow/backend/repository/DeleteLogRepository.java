0bac35dbaefc014e5382d08dc037e3fad693e1ce
mijinn0 <pmj_0818@naver.com>
2025-10-27 16:12:10 +0900

    refactor: 퇴사 통계 로직 수정

--- src/main/java/com/logi_flow/backend/repository/DeleteLogRepository.java
+++ src/main/java/com/logi_flow/backend/repository/DeleteLogRepository.java
@@ -1,12 +1,16 @@
 package com.logi_flow.backend.repository;
 
+import com.logi_flow.backend.common.enums.TableRef;
+import com.logi_flow.backend.common.enums.driver.DriverStatus;
 import com.logi_flow.backend.entity.DeleteLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface DeleteLogRepository extends JpaRepository<DeleteLog, Long> {
     Optional<DeleteLog> findByTableNameAndRecordId(String tableName, Long recordId);
+    long countByTableNameAndCreatedAtGreaterThanEqualAndCreatedAtLessThan(String tableName, LocalDateTime start, LocalDateTime end);
 }
